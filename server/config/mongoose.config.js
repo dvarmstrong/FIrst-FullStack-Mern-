910d78ae39646526893c19ec65e4a403d482993d
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-06-13 13:47:29 -0400

    Added the front end and the backend

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+const mongoose = require ('mongoose');
+
+
+
+mongoose.connect("mongodb://127.0.0.1/crmdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Established a connection the database"))
+    .catch(err => console.log('Something went wrong when connecting to the database', err));
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = 8000;
 
+//**Link the  person route to the server */
+require('./config/mongoose.config')
+const personRoutes = require("./routes/person.routes");
+personRoutes(app);
 
 app.listen(port, () => console.log('Listening on port: ${port}'));
